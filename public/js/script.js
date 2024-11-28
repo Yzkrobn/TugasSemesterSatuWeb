6a9aa3c6b9ba656b058d0fa3b3dede62e9aa97d2
Yzkrobn <oktavianbagasnugroho@gmail.com>
2024-11-29 05:23:46 +0700

    Update 2

---
+++ public/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const nB = document.getElementById("navButton");
+  const nC = document.getElementById("navController");
+  const l1 = document.getElementById("line1");
+  const l2 = document.getElementById("line2");
+  const l3 = document.getElementById("line3");
+  const dDB = document.getElementById("dropDownButton");
+  const dD = document.getElementById("dropDown");
+  const dDL1 = document.getElementById("dropDownList-1");
+  const dDL2 = document.getElementById("dropDownList-2");
+  const dDL3 = document.getElementById("dropDownList-3");
+  const dDL4 = document.getElementById("dropDownList-4");
+  const dDL5 = document.getElementById("dropDownList-5");
+
+  const navSide = () => {
+    if (nB && nC && l1 && l2 && l3) {
+      nB.addEventListener("click", () => {
+        nC.classList.toggle("w-0");
+        nC.classList.toggle("w-36");
+        l1.classList.toggle("rotate-45");
+        l2.classList.toggle("opacity-0");
+        l2.classList.toggle("invisible");
+        l3.classList.toggle("-rotate-45");
+      });
+    }
+  };
+  navSide();
+  const dropDown = () => {
+    if(dDB && dD){
+      dDB.addEventListener("click", () =>{
+        dD.classList.toggle("hidden")
+        setTimeout(() => {
+          dDL1.classList.toggle("")
+        }, 100);
+      })
+    }
+  }
+  dropDown();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [ "./public/**/*.html",
+    "./public/js/**/*.js",
+    "./src/css/**/*.css",],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth:{
+        default:'1px',
+        '0': '0px',
+        '0.5':'0.5px',
+        '1':'1px',
+        '2': '2px',
+      }
+    },
   },
   plugins: [],
 }
-
