86087a97612e6df8a88eeedad5f2e26828fc7f3e
Yzkrobn <oktavianbagasnugroho@gmail.com>
2024-12-01 17:55:57 +0700

    Update 4

--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const nB = document.getElementById("navButton");
-  const nC = document.getElementById("navController");
-  const l1 = document.getElementById("line1");
-  const l2 = document.getElementById("line2");
-  const l3 = document.getElementById("line3");
-  const dDB = document.getElementById("dropDownButton");
-  const dD = document.getElementById("dropDown");
-  const dDL1 = document.getElementById("dropDownList-1");
-  const dDL2 = document.getElementById("dropDownList-2");
-  const dDL3 = document.getElementById("dropDownList-3");
-  const dDL4 = document.getElementById("dropDownList-4");
-  const dDL5 = document.getElementById("dropDownList-5");
+  const navButton = document.getElementById("navButton");
+  const navLink = document.getElementById("navContent");
+  const listLines = [
+    document.getElementById("listLine-1"),
+    document.getElementById("listLine-2"),
+    document.getElementById("listLine-3"),
+  ];
+  const dropdowns = document.querySelectorAll("[id^='dropDownButton-']");
+  
+  const toggleDropdown = (content, icon) => {
+    if (content.style.maxHeight) {
+      content.style.transition = "max-height 0.5s ease-out";
+      content.style.maxHeight = null;
+    } else {
+      content.style.transition = "max-height 0.5s ease-in-out";
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+    icon.classList.toggle("rotate-180");
+  };
+
+  const closeAllDropdowns = () => {
+    dropdowns.forEach((button) => {
+      const id = button.id.split("-")[1];
+      const content = document.getElementById(`dropDownContent-${id}`);
+      const icon = document.getElementById(`dropDownIcon-${id}`);
+      if (content && icon && content.style.maxHeight) {
+        content.style.maxHeight = null;
+        icon.classList.remove("rotate-180");
+      }
+    });
+  };
 
   const navSide = () => {
-    if (nB && nC && l1 && l2 && l3) {
-      nB.addEventListener("click", () => {
-        nC.classList.toggle("w-0");
-        nC.classList.toggle("w-36");
-        l1.classList.toggle("rotate-45");
-        l2.classList.toggle("opacity-0");
-        l2.classList.toggle("invisible");
-        l3.classList.toggle("-rotate-45");
+    if (navButton && navLink && listLines.every((line) => line)) {
+      navButton.addEventListener("click", () => {
+        navLink.classList.toggle("w-0");
+        navLink.classList.toggle("w-36");
+        listLines[0].classList.toggle("rotate-45");
+        listLines[1].classList.toggle("opacity-0");
+        listLines[1].classList.toggle("invisible");
+        listLines[2].classList.toggle("-rotate-45");
+
+        closeAllDropdowns();
       });
     }
   };
   navSide();
-  const dropDown = () => {
-    if(dDB && dD){
-      dDB.addEventListener("click", () =>{
-        dD.classList.toggle("hidden")
-        setTimeout(() => {
-          dDL1.classList.toggle("opacity-100")
-        }, 100);
-        setTimeout(() => {
-          dDL2.classList.toggle("opacity-100")
-        }, 200);
-        setTimeout(() => {
-          dDL3.classList.toggle("opacity-100")
-        }, 300);
-        setTimeout(() => {
-          dDL4.classList.toggle("opacity-100")
-        }, 400);
-        setTimeout(() => {
-          dDL5.classList.toggle("opacity-100")
-        }, 500);
-      })
+
+  dropdowns.forEach((button) => {
+    const id = button.id.split("-")[1];
+    const content = document.getElementById(`dropDownContent-${id}`);
+    const icon = document.getElementById(`dropDownIcon-${id}`);
+
+    if (content && icon) {
+      button.addEventListener("click", () => toggleDropdown(content, icon));
     }
-  }
-  dropDown();
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./public/**/*.html",
+  content: [
+    "./public/**/*.html",
     "./public/js/**/*.js",
-    "./src/css/**/*.css",],
+    "./src/css/**/*.css",
+  ],
   theme: {
     extend: {
-      borderWidth:{
-        default:'1px',
+      borderWidth: {
+        default: '1px',
         '0': '0px',
-        '0.5':'0.5px',
-        '1':'1px',
+        '0.5': '0.5px',
+        '1': '1px',
         '2': '2px',
+      },
+      animation: {
+        'scroll-infinite': 'scroll-horizontal 30s linear infinite',
+      },
+      keyframes: {
+        'scroll-horizontal': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-1700%)' },
+        },
+      },
+      screens:{
+        'xs':''
       }
     },
   },
   plugins: [],
-}
+};
